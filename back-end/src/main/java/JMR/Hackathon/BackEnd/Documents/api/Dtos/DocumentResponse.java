952515c9051a1a29177758784880804fc9797829
Valentpoz <146148786+Valenpoz@users.noreply.github.com>
2026-07-23 19:22:25 -0500

    Actualizar  campos

--- back-end/src/main/java/JMR/Hackathon/BackEnd/Documents/api/Dtos/DocumentResponse.java
+++ back-end/src/main/java/JMR/Hackathon/BackEnd/Documents/api/Dtos/DocumentResponse.java
@@ -1,12 +1,15 @@
 package JMR.Hackathon.BackEnd.Documents.api.Dtos;
 
-
 import lombok.Builder;
+
+import java.util.List;
 
 @Builder
 public record DocumentResponse(
         String title,
-        String content
-
-) {
-}
+        String content,
+        String categoria,
+        Float  probabilidadCategoria,
+        String nivel,
+        List<String> keywords
+) {}
