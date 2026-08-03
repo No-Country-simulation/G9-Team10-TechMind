4034eac6cf84b3f44f820fa5cc8c85f4fe52ff07
JM29R <juanmanuelrios5602@gmail.com>
2026-08-03 15:42:01 -0300

    MIGRATIOS INICIALES FINALES:)

--- back-end/src/main/java/JMR/Hackathon/BackEnd/BackEndApplication.java
+++ back-end/src/main/java/JMR/Hackathon/BackEnd/BackEndApplication.java
@@ -2,10 +2,9 @@ package JMR.Hackathon.BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class BackEndApplication {
+public class BackEndApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
